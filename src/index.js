01c7b8a0ccc12f25e68245988ab3477e80cf6750
Gert <gert@bildly.eu>
2023-06-30 09:46:00 +0300

    aa

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { $, createSVG, createCanvas } from './svg_utils';
 import Bar from './bar';
 import Arrow from './arrow';
 import Popup from './popup';
-import Names from './names';
+// import Names from './names';
 
 import './gantt.scss';
 
@@ -1086,4 +1086,62 @@ Gantt.VIEW_MODE = VIEW_MODE;
 
 function generate_id(task) {
     return task.name + '_' + Math.random().toString(36).slice(2, 12);
+}
+
+class Names {
+    constructor(gantt, task) {
+        this.task = task;
+        this.gantt = gantt;
+        this.make();
+    }
+
+    makeHeader() {
+        const header = document.createElement('div');
+        const headerText = document.createElement('span');
+        header.classList.add('header');
+        header.style.height = this.gantt.options.header_height + 10 + 'px';
+        headerText.innerText = this.gantt.options.custom_names.header;
+
+        header.appendChild(headerText);
+
+        return header;
+    }
+
+    make() {
+
+        const row_height =
+            this.gantt.options.bar_height + this.gantt.options.padding;
+
+        const parentDiv = document.createElement('div');
+        const taskDiv = document.createElement('div');
+
+        const nameField = document.createElement('span');
+
+        // nameField.innerText = this.task?.name;
+        nameField.innerText = 'test';
+
+        parentDiv.classList.add('tasks');
+
+        taskDiv.classList.add('task');
+        taskDiv.style.height = `${row_height}px`;
+        taskDiv.setAttribute('data-value', this.task?.id);
+        taskDiv.appendChild(nameField);
+
+        this.gantt.options.custom_names.buttons?.forEach(button => {
+            const actionButton = document.createElement('button');
+            actionButton.innerText = button.label;
+            $.on(actionButton, 'click', (e) => {
+                this.gantt.trigger_event(button?.trigger, [this.task]);
+            });
+
+            parentDiv.appendChild(actionButton);
+        })
+
+        parentDiv.append(taskDiv);
+ 
+
+        return parentDiv;
+
+    }
+
 }
