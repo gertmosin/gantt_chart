ee663defc260ac5e27c162e7ed24f0d2bb4f28e2
Gert <gert@bildly.eu>
2023-06-30 09:37:22 +0300

    yes

--- src/names.js
+++ src/names.js
@@ -1,13 +1,6 @@
 import { $ } from './svg_utils';
 
 export default class Names {
-    // constructor(parent, custom_html, options, tasks, gantt) {
-    //     this.parent = parent;
-    //     this.custom_html = custom_html;
-    //     this.gantt = gantt;
-    //     this.make(options, tasks);
-    //     console.log(tasks);
-    // }
     constructor(gantt, task) {
         this.task = task;
         this.gantt = gantt;
@@ -18,7 +11,6 @@ export default class Names {
         const header = document.createElement('div');
         const headerText = document.createElement('span');
         header.classList.add('header');
-        // const row_height = this.gantt.options.bar_height + this.gantt.options.padding;
         header.style.height = this.gantt.options.header_height + 10 + 'px';
         headerText.innerText = this.gantt.options.custom_names.header;
 
@@ -28,28 +20,9 @@ export default class Names {
     }
 
     make() {
-        // this.parent.innerHTML = `
-        //     <div class="header"><span>Items</span></div>
-        //     <div class="tasks"></div>
-        // `;
 
-        // this.hide();
-        // this.header = this.parent.querySelector('.header');
-        // this.tasksElement = this.parent.querySelector('.tasks');
-        // this.tasksElement.style.display = 'flex';
-        // this.tasksElement.style.flexDirection = 'column';
-        // this.title = this.parent.querySelector('.title');
-        // this.subtitle = this.parent.querySelector('.subtitle');
-        // this.pointer = this.parent.querySelector('.pointer');
-
-        // console.log(this.tasksElement);
-
-        // const taskElements = [];
         const row_height =
             this.gantt.options.bar_height + this.gantt.options.padding;
-
-        // for (let i = 0; i < tasks?.length; i++) {
-        // console.log(tasks[i]);
 
         const parentDiv = document.createElement('div');
         const taskDiv = document.createElement('div');
@@ -75,142 +48,11 @@ export default class Names {
             parentDiv.appendChild(actionButton);
         })
 
-        // b1.textContent = this.gantt.options.custom_names.b1.label;
-        // b2.textContent = this.gantt.options.custom_names.b2.label;
-        // b3.textContent = this.gantt.options.custom_names.b3.label;
-        //
-        // $.on(b1, 'click', (e) => {
-        //     // console.log(this.gantt);
-        //     this.gantt.trigger_event('action1', [this.task]);
-        // });
-        //
-        // $.on(b2, 'click', (e) => {
-        //     // console.log(this.gantt);
-        //     this.gantt.trigger_event('action2', [this.task]);
-        // });
-        //
-        // $.on(b3, 'click', (e) => {
-        //     // console.log(this.gantt);
-        //     this.gantt.trigger_event('action3', [this.task]);
-        // });
-
         parentDiv.append(taskDiv);
-        // parentDiv.append(editButton, taskDiv);
+ 
 
         return parentDiv;
 
-        return `<div><button id="task_button">trigger</button><div class='task' data-value='${this.task.id}' style='height: ${row_height}px;'><span>${this.task.name}</span></div></div>`;
-
-        // console.log(emptyElement);
-        // const taskElement = document.createElement('span');
-        // taskElement.classList.add('single-task');
-        // taskElement.innerText = tasks[i].name;
-        // this.tasksElement.append(taskElement);
-        // this.tasksElement.innerHTML = emptyElement;
-        // }
-
-        // console.log(this.$svg);
-        // this.tasksElement.innerHTML = taskElements.join(' ');
-        // this.header.style.height = options.header_height + 10 + 'px';
-        // this.title.innerText = 'asdjasdasdasd';
-
-        // const taskButton = this.parent.querySelector('#task_button');
-        // $.on(taskButton, 'click', (e) => {
-        //     console.log(this.gantt);
-        //     this.gantt.trigger_event('edit', [this.gantt.get_task]);
-        // });
     }
 
-    // make(options, tasks) {
-    //     this.parent.innerHTML = `
-    //         <div class="header"><span>Items</span></div>
-    //         <div class="tasks"></div>
-    //     `;
-
-    //     // this.hide();
-    //     this.header = this.parent.querySelector('.header');
-    //     this.tasksElement = this.parent.querySelector('.tasks');
-    //     // this.tasksElement.style.display = 'flex';
-    //     // this.tasksElement.style.flexDirection = 'column';
-    //     // this.title = this.parent.querySelector('.title');
-    //     // this.subtitle = this.parent.querySelector('.subtitle');
-    //     // this.pointer = this.parent.querySelector('.pointer');
-
-    //     // console.log(this.tasksElement);
-
-    //     const taskElements = [];
-    //     const row_height = options.bar_height + options.padding;
-
-    //     for (let i = 0; i < tasks?.length; i++) {
-    //         // console.log(tasks[i]);
-    //         taskElements.push(
-    //             `<div><button id="task_button">trigger</button><div class='task' data-value='${tasks[i].id}' style='height: ${row_height}px;'><span>${tasks[i].name}</span></div></div>`
-    //         );
-    //         // console.log(emptyElement);
-    //         // const taskElement = document.createElement('span');
-    //         // taskElement.classList.add('single-task');
-    //         // taskElement.innerText = tasks[i].name;
-    //         // this.tasksElement.append(taskElement);
-    //         // this.tasksElement.innerHTML = emptyElement;
-    //     }
-
-    //     console.log(this.$svg);
-    //     this.tasksElement.innerHTML = taskElements.join(' ');
-    //     this.header.style.height = options.header_height + 10 + 'px';
-    //     // this.title.innerText = 'asdjasdasdasd';
-
-    //     const taskButton = this.parent.querySelector('#task_button');
-    //     $.on(taskButton, 'click', (e) => {
-    //         console.log(this.gantt);
-    //         this.gantt.trigger_event('edit', [this.gantt.get_task]);
-    //     });
-    // }
-
-    // show(options) {
-    //     if (!options.target_element) {
-    //         throw new Error('target_element is required to show popup');
-    //     }
-    //     if (!options.position) {
-    //         options.position = 'left';
-    //     }
-    //     const target_element = options.target_element;
-
-    //     if (this.custom_html) {
-    //         let html = this.custom_html(options.task);
-    //         html += '<div class="pointer"></div>';
-    //         this.parent.innerHTML = html;
-    //         this.pointer = this.parent.querySelector('.pointer');
-    //     } else {
-    //         // set data
-    //         this.title.innerHTML = options.title;
-    //         this.subtitle.innerHTML = options.subtitle;
-    //         this.parent.style.width = this.parent.clientWidth + 'px';
-    //     }
-
-    //     // set position
-    //     let position_meta;
-    //     if (target_element instanceof HTMLElement) {
-    //         position_meta = target_element.getBoundingClientRect();
-    //     } else if (target_element instanceof SVGElement) {
-    //         position_meta = options.target_element.getBBox();
-    //     }
-
-    //     if (options.position === 'left') {
-    //         this.parent.style.left =
-    //             position_meta.x + (position_meta.width + 10) + 'px';
-    //         this.parent.style.top = position_meta.y + 'px';
-
-    //         this.pointer.style.transform = 'rotateZ(90deg)';
-    //         this.pointer.style.left = '-7px';
-    //         this.pointer.style.top = '2px';
-    //     }
-
-    //     // show
-    //     this.parent.style.opacity = 1;
-    // }
-
-    // hide() {
-    //     this.parent.style.opacity = 0;
-    //     this.parent.style.left = 0;
-    // }
 }
