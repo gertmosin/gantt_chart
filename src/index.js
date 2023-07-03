aa612eacdf713cd51d9fa2712d1e59fc9b840c6d
Gert <gert@bildly.eu>
2023-07-03 14:33:15 +0300

    aa

--- src/index.js
+++ src/index.js
@@ -665,7 +665,7 @@ export default class Gantt {
     }
 
     make_names() {
-        this.$titles.childNodes = [];
+        this.$titles.children = [];
         const header = new Names(this);
         this.$titles.appendChild(header.makeHeader());
         this.names = this.tasks.map((task) => {
